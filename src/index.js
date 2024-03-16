d8ab67358e9e2a089dc36083c5d9c0ee3939cd4f
Trubnikov Dima <trubnikov.dima999@gmail.com>
2024-03-16 13:44:43 +0200

    second commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// data-visualization-components.js
+
+// Function to create a bar chart
+function createBarChart(data, options) {
+    // Implementation for creating a bar chart
+  }
+  
+  // Function to create a line chart
+  function createLineChart(data, options) {
+    // Implementation for creating a line chart
+  }
+  
+  // Function to create a pie chart
+  function createPieChart(data, options) {
+    // Implementation for creating a pie chart
+  }
+  
+  // Exporting data visualization components
+  module.exports = {
+    createBarChart,
+    createLineChart,
+    createPieChart
+  };
+  
